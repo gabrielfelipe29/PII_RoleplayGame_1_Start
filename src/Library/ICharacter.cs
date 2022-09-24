d3136ff45e597b1f39085a5827e8621d02a48301
tomasdarracq <tomasdarracq12@gmail.com>
2022-09-24 08:53:04 -0300

    Update ICharacter.cs

--- src/Library/ICharacter.cs
+++ src/Library/ICharacter.cs
@@ -2,5 +2,16 @@ namespace Library;
 using System;
 public interface ICharacter
 {
-    
+    public string Name { get; set; }
+    public string Description { get; set; } 
+    public double InitialHealth {get;set;}
+    public double ActualHealth {get;set;}
+    public Items AttackItem {get;set;}
+    public Items DefenseItem {get;set;}
+public void Attack( ICharacter otherCharacter){
+    //
+}
+public void Cure(){
+    //
+}
 }
